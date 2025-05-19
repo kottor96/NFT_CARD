4e6b5b6e992f2f3d77fe519664743cc39d335856
kottor <gardien243@gmail.com>
2025-05-20 00:03:18 +0200

    pour gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
-import  Card from './componants/card'
+import img1 from './src/assets/image-equilibrium.jpg'
+import img2 from "./src/assets/image-avatar.png"
+import img3 from "./src/assets/icon-view.svg"
+import Card from './componants/card'
 
 function App() {
 
@@ -8,12 +11,12 @@ function App() {
       <Card 
         titre='Equilibrium #3429' 
         p='Our Equilibrium collection promotes balance and calm.' 
-        img1='./src/assets/image-equilibrium.jpg' 
-        img2='./src/assets/image-avatar.png' 
+        img1={img1} 
+        img2={img2}
         nbr1={0.041} 
         nbr2={3} 
         nom="Jules Wyvern"
-        img3='./src/assets/icon-view.svg'
+        img3={img3}
       />
     </>
   )
